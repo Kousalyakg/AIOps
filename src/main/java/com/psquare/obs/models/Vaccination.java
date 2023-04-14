c9c0dc8aaeb70d1ee411026544053ce343e5b4fb
Kousalya <kousalya@truecover.net>
2023-04-14 17:11:43 +0800

    added h2 db lambok

--- src/main/java/com/psquare/obs/models/Vaccination.java
+++ src/main/java/com/psquare/obs/models/Vaccination.java
@@ -20,6 +20,55 @@ public class Vaccination {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String memberId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getVacName() {
+        return vacName;
+    }
+
+    public void setVacName(String vacName) {
+        this.vacName = vacName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
     private String vacName;
     private String date;
     private Double amount;
